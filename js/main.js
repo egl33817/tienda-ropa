0d94135e0ecc68cd53d4b1ca1c45e28388eb85cb
egl33817 <egl33817@educastur.es>
2025-11-27 16:41:39 +0100

    Creamos una función JS llamada cargarProductos que coge los datos de los productos que tenemos en el array productos dentro de main.js y los coloca en el div contenedor-productos de index.html

--- js/main.js
+++ js/main.js
@@ -184,3 +184,30 @@ const productos = [
         precio: 1000
     }
 ];
+
+// Elementos del DOM.
+// getElementById -> permite seleccionar un elemento del DOM usando su atributo "id".
+// querySelector -> permite seleccionar elementos del DOM usando selectores CSS (id, clase, etiqueta, atributos...).
+const contenedorProductos = document.querySelector("#contenedor-productos")
+
+function cargarProductos() 
+{
+    productos.forEach(producto => {
+        const divProducto = document.createElement("div")
+        
+        divProducto.classList.add("producto")
+
+        divProducto.innerHTML = `
+            <img class="producto-imagen" src="${producto.imagen}" alt="${producto.titulo}" />
+            <div class="producto-detalles">
+                <h3 class="producto-titulo">${producto.titulo}</h3>
+                <p class="producto-precio">$${producto.precio}</p>
+                <button class="producto-agregar" id="${producto.id}">Agregar</button>
+            </div>
+        `
+
+        contenedorProductos.append(divProducto)
+    })
+}
+
+cargarProductos()
